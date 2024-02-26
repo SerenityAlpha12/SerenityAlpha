d65a728c60cddfcb67b40c662954d6bd759f6a6f
efexng <efeapoki2020@gmail.com>
2024-02-26 18:02:13 +0000

    chnages

--- Storage/script.js
+++ Storage/script.js
@@ -17,3 +17,44 @@ var swiper = new Swiper(".mySwiper", {
     content.style.display = (content.style.display === 'block') ? 'none' : 'block';
   }
 
+
+
+function togglePasswordVisibility(inputId) {
+const passwordInput = document.getElementById(inputId);
+const toggleIcon = document.getElementById('toggle-password');
+
+if (passwordInput.type === 'password') {
+   passwordInput.type = 'text';
+   toggleIcon.classList.add('fa-eye');
+   toggleIcon.classList.remove('fa-eye-slash');
+   passwordInput.focus(); // Set focus to the input when showing the password
+} else {
+   passwordInput.type = 'password';
+   toggleIcon.classList.add('fa-eye-slash');
+   toggleIcon.classList.remove('fa-eye');
+   passwordInput.focus(); // Set focus to the input when hiding the password
+}
+}
+
+function togglePasswordVisibility(inputId) {
+const passwordInput = document.getElementById(inputId);
+const toggleIcon = document.getElementById('toggle-password');
+
+if (passwordInput.type === 'confirmpassword') {
+   passwordInput.type = 'text';
+   toggleIcon.classList.add('fa-eye');
+   toggleIcon.classList.remove('fa-eye-slash');
+   passwordInput.focus(); // Set focus to the input when showing the password
+} else {
+   passwordInput.type = 'password';
+   toggleIcon.classList.add('fa-eye-slash');
+   toggleIcon.classList.remove('fa-eye');
+   passwordInput.focus(); // Set focus to the input when hiding the password
+}
+}
+
+
+function validatePhoneNumber(input) {
+  // Remove any non-numeric or non-special characters
+  input.value = input.value.replace(/[^0-9#+*]/g, '');
+}
