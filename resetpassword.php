2c712bee33faf58f9900bdf39ccbf1ff2a3729a8
efexng <efeapoki2020@gmail.com>
2024-05-06 14:09:16 +0100

    changes

--- resetpassword.php
+++ resetpassword.php
@@ -73,7 +73,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     $mail->Host = 'smtp.gmail.com'; // Specify main and backup SMTP servers
                     $mail->SMTPAuth   = true;
                     $mail->Username   = 'serenityalpha2024@gmail.com'; // Replace with your SMTP username
-                    $mail->Password   = 'qrpjiunrqhrohvpj'; // Replace with your SMTP password
+                    $mail->Password   = ''; // Replace with your SMTP password
                     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // Enable TLS encryption, `ssl` also accepted
                     $mail->Port       = 587; // You may use 465 instead for 'ssl'
 
